d3366340c411a453c1f73e1c7780153f3cad353f
KTMichael <kmpg1822@gmail.com>
2021-10-13 16:49:36 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,33 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
+
 
 USE chat;
 
-CREATE TABLE messages (
+CREATE TABLE users (
   /* Describe your table here.*/
+  id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(10)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 
+  text VARCHAR(160),
+
+  roomname VARCHAR(15),
+
+  user_ID INT,
+   FOREIGN KEY(user_ID) REFERENCES users(id)
+
+);
+
+
+-- CREATE TABLE rooms (
+--   /* Describe your table here.*/
+-- );
+/* Create other tables and define schemas for them here! */
 
 
 
