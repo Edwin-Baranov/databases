dca6273a8fc8e129f80bfbe9e885bcc37f81b9e0
KTMichael <kmpg1822@gmail.com>
2021-10-14 12:35:36 -0700

    Working on Controllers and Models

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -2,5 +2,17 @@ var models = require('../models');
 
 module.exports = {
   get: function (req, res) {}, // a function which handles a get request for all messages
-  post: function (req, res) {} // a function which handles posting a message to the database
+  post: function (req, res) {
+    let messageParams = (req.body.username, req.body.text, req.body.roomname);
+    models.messages.create(messageParams, (error, results) => {
+      if (error) {
+        console.log('error with post');
+        res.sendStatus(404, results);
+      } else {
+        console.log('great job post works!');
+        res.sendStatus(201);
+      }
+    });
+
+  } // a function which handles posting a message to the database
 };
--- server/models/messages.js
+++ server/models/messages.js
@@ -1,6 +1,32 @@
 var db = require('../db');
 
+//db.connection
 module.exports = {
-  getAll: function () {}, // a function which produces all the messages
-  create: function () {} // a function which can be used to insert a message into the database
+  getAll: function (callback) {
+    // let queryVal = "SELECT messages.id, messages.text, messages.roomname, messages.user_ID FROM messages INNER JOIN users ON (messages.user_ID = users.id)";
+    // db.connection.query(queryVal, (error, results) => {
+    //   if (error) {
+    //     callback(error, results);
+    //   }
+    // });
+  }, // a function which produces all the messages
+  create: function (newMessageParams, callback) {
+    // let userID;
+    // db.connection.query('SELECT id FROM users WHERE username=' + username + ';', (err, userID) => {
+    //   newMessageParams.user_ID = userID;
+    //   db.connection.query(queryVal, newMessageParams, (error, results) => {
+    //     callback(error, results);
+    //   });
+    // })
+
+
+
+
+
+    // let userID = "SELECT id FROM users WHERE username = ?'' ;
+    let queryVal = "INSERT INTO messages (user_ID, text, roomname,) VALUE ((SELECT id FROM users WHERE username = ?), ?, ?)";
+    db.connection.query(queryVal, newMessageParams, (error, results) => {
+      callback(error, results);
+    });
+  } // a function which can be used to insert a message into the database
 };
